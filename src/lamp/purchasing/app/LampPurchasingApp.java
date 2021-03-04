928ffcd85f9f7a5b0a6d2791a47537f6bb872662
Shashwat Kushwaha <vasuk8324@gmail.com>
2021-03-04 17:32:42 +0530

    Basic Structure added

--- src/lamp/purchasing/app/LampPurchasingApp.java
+++ src/lamp/purchasing/app/LampPurchasingApp.java
@@ -1,11 +1,41 @@
 
 package lamp.purchasing.app;
 
+import java.util.Scanner;
+
 public class LampPurchasingApp {
 
     
     public static void main(String[] args) {
         
+        System.out.print("Press\n"+
+                         "1-Signup\n"+
+                         "2-SigIn\n"+
+                         "3-Lamp Store\n");
+        
+        Integer x;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        
+        switch(x)
+        {
+            case 1:
+                    //signup
+//                System.out.println(x);
+                     break;
+            case 2:
+                   //sigin
+//                System.out.println(x);
+                    break;
+            case 3:
+                   //Lamp Store
+//                System.out.println(x);
+                    break;
+            default:
+                    break;
+                  
+        
+        }
     }
     
 }
